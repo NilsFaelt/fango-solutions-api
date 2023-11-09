62d6259caaca5cc5883990423cb223c18bb0e1a0
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-11-09 14:31:21 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { execSync } from 'child_process';
-
+//
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   if (process.env.NODE_ENV === 'development') {
