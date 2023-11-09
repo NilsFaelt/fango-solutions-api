1398ddfd8bab4900929677645ecbe86a40db96ec
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-11-09 11:39:29 +0100

    test

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -1,7 +1,7 @@
 const firebaseConfig = {
   clientEmail: process.env.CLIENT_EMAIL,
   projectId: process.env.PROJECT_ID,
-  privateKey: process.env.PRIVATE_KEY,
+  privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
   // databaseUrl: process.env.CLIENT_EMAIL,
 };
 
