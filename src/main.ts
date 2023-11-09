47e8c23f4f939ac9b63d29936278721320a91c56
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-11-09 14:23:20 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { execSync } from 'child_process';
 
 async function bootstrap() {
+  execSync(
+    'npm install && npx prisma migrate save --name init && npx prisma migrate up && npm start ',
+  );
   const app = await NestFactory.create(AppModule, { cors: true });
   if (process.env.NODE_ENV === 'development') {
     app.enableCors({ origin: '*' });
