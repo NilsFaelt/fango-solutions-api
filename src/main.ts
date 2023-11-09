875c20ff46d3fa31fa2b875bc39d497e605be6d5
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-11-09 14:25:04 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { execSync } from 'child_process';
 
 async function bootstrap() {
-  execSync(
-    'npm install && npx prisma migrate save --name init && npx prisma migrate up && npm start ',
-  );
   const app = await NestFactory.create(AppModule, { cors: true });
   if (process.env.NODE_ENV === 'development') {
     app.enableCors({ origin: '*' });
   } else {
+    execSync(
+      'npm install && npx prisma migrate save --name init && npx prisma migrate up && npm start ',
+    );
     app.enableCors({
       origin: 'https://fangosolutions.com',
       methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
