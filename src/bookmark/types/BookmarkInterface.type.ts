223aad82f6dccc9216bd5ea039b8dde0c5717855
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-11-10 11:45:42 +0100

    test

--- src/bookmark/types/BookmarkInterface.type.ts
+++ src/bookmark/types/BookmarkInterface.type.ts
@@ -1,18 +1,6 @@
 import { ContentInterface } from 'src/content';
 import { PaginationInterface, BokmarkAnalyticsInterface } from 'src/types';
 
-export interface BookmarkInterface {
-  children?: ChildInterface[];
-  analytics?: BokmarkAnalyticsInterface;
-  pagination?: PaginationInterface;
-  content?: ContentInterface[];
-  userEmail: string;
-  id: string;
-  url: string;
-  alias: string;
-  createdAt: Date;
-}
-
 export interface ChildInterface {
   id: string;
   url: string;
@@ -20,4 +8,12 @@ export interface ChildInterface {
   updatedAt: Date;
   deletedAt: Date;
   bookmarkId: string;
+}
+export interface BookmarkInterface {
+  children?: ChildInterface[];
+  analytics?: BokmarkAnalyticsInterface;
+  pagination?: PaginationInterface;
+  content?: ContentInterface[];
+
+  createdAt: Date;
 }
